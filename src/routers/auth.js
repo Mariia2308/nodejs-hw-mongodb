b738b15dd470ec69aa323203b8fad939e801a53c
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-07-06 15:34:17 +0300

    chipichipi chapachapa

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,7 +5,7 @@ import { loginUserController,  registerUserController } from "../controllers/aut
 import { validateBody } from "../middlewares/validateBody.js"; 
 import { registerUserSchema } from "../validations/registerUserSchema.js";
 import { loginUserSchema } from "../validations/loginSchema.js";
-import { RefreshTokenController } from "../controllers/auth.js";
+import { refreshTokenController } from "../controllers/auth.js";
 import { logoutUserController } from "../controllers/auth.js";
 
 
@@ -14,7 +14,7 @@ const authRouter = Router();
 
 authRouter.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
 authRouter.post('/login', ctrlWrapper(loginUserController), validateBody(loginUserSchema));
-authRouter.post('/reset-token', ctrlWrapper(RefreshTokenController), validateBody());
+authRouter.post('/reset-token', ctrlWrapper(refreshTokenController), validateBody());
 authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 export default authRouter;
